4ac3e8c7ce9f0b5d73a1e7721945b42b7750ea24
dell <dell@DESKTOP-99M7JDM>
2023-11-08 12:29:40 -0800

    crm

--- crm2/src/main/java/com/PageObjects/Homepage.java
+++ crm2/src/main/java/com/PageObjects/Homepage.java
@@ -1,5 +1,67 @@
 package com.PageObjects;
 
-public class Homepage {
+import org.apache.xmlbeans.UserType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-}
+import com.base.Testbase;
+import com.utils.Utils;
+
+public class Homepage extends Testbase {
+
+public Homepage() throws Throwable {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@FindBy(xpath="//i[contains(@class,'fa-houzz')]")
+	WebElement Inventory;
+	@FindBy(xpath="//ul[@class='treeview-menu']/li/a[text()='All Inventory']")
+	WebElement AllInventory;
+	@FindBy(xpath="//ul[@class='treeview-menu']/li/a[contains(text(),'Move Stock')]")
+	WebElement Movestock;
+	@FindBy(xpath="//i[contains(@class,'fa-database')]")
+	WebElement master; 
+		@FindBy(xpath="//a[@href='/Master/Usertypes']")
+		WebElement usertype;
+    public Homepage(WebDriver driver)throws Throwable
+    {
+    	PageFactory.initElements(driver,this);
+    }
+    public  Inventory inventory() throws Throwable
+    {
+    	Utils.moveToElement(Inventory);
+    	AllInventory.click();	 
+		return  new Inventory();
+	}
+
+    public   Movestock movestock() throws Throwable
+    {
+    	Utils.moveToElement(Inventory);
+    	Movestock.click();
+		return new Movestock();
+		 
+    
+		
+	}
+	public void holidays() {
+		// TODO Auto-generated method stub
+		
+	}
+ 
+	
+ 
+ 
+  
+   
+	
+
+		
+	}
+ 
+
+	
+	
+
+
--- crm2/src/main/java/com/utils/Utils.java
+++ crm2/src/main/java/com/utils/Utils.java
@@ -1,22 +1,38 @@
 package com.utils;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.base.Testbase;
 
 	public class Utils extends Testbase {
+	
 		public Utils() throws Throwable {
 			super();
 
 		}
 
-		public static Object[][] ReadDataFromExcel(String sheetname) throws Throwable {
-			File file = new File("./src/main/java/com/testdata/data.xlsx");
+		public static Object[][] ReadDataFromExcel(String sheetname) throws IOException{
+			File file = new File("./src/main/java/com/testdata/crmtestdata.xlsx");
 
 			FileInputStream stream = new FileInputStream(file);
 
@@ -28,7 +44,7 @@ import com.base.Testbase;
 
 			int cellnum = sheet.getRow(1).getLastCellNum();
 
-			Object[][] data = new Object[rownum - 1][cellnum];
+			String[][] data = new String[rownum - 1][cellnum];
 
 			for (int i = 0; i < rownum - 1; i++) {
 
@@ -43,7 +59,87 @@ import com.base.Testbase;
 			}
 			return data;
 		}
-	}
+	
 
+	 public static Object[][] readDataFromExcel(String sheetname) throws IOException {
+		    FileInputStream fis = new FileInputStream("./src/main/java/com/testdata/crmtestdata.xlsx");
+		    XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		    XSSFSheet sheet = workbook.getSheet(sheetname);
 
+		    int numRows = sheet.getPhysicalNumberOfRows();
+		    int numCols = sheet.getRow(0).getLastCellNum();
+
+		    List<Object[]> dataRows = new ArrayList<>();
+
+		    for (int i = 1; i < numRows; i++) {  
+		       Row row = sheet.getRow(i);
+		        Object[] rowData = new Object[numCols];
+		        boolean isEmptyRow = true;
+
+		        for (int j = 0; j < numCols; j++) {
+		            Cell cell = row.getCell(j);
+		            if (cell != null) {
+		                DataFormatter format = new DataFormatter();
+		                rowData[j] = format.formatCellValue(cell);
+		                if (!cell.toString().trim().isEmpty()) {
+		                    isEmptyRow = false;
+		                }
+		            }
+		        }
+
+		        if (!isEmptyRow) {
+		            dataRows.add(rowData);
+		        }
+		    }
+
+		    Object[][] data = dataRows.toArray(new Object[0][]);
+
+		    return data;
+		}
+
+	 
+	//select by value
+ public static void dropdown(WebElement wb,String value)
+ {
+ 	Select s=new Select(wb);
+ 	s.selectByValue(value);
+ }
+ //select by index
+ public static void dropdown(WebElement wb,int index)
+ {
+ 	Select s=new Select(wb);
+ 	s.selectByIndex(index);
+ }
+ //select y visible text
+ public static void dropdownBy(String text,WebElement wb)
+ {	
+ 	Select s=new Select(wb);
+ 	s.selectByVisibleText(text);
+ }
+ public static void moveToElement(WebElement wb)
+ {
+ 	Actions act=new Actions(driver);
+ 	act.moveToElement(wb).click().perform();
+ }
+public static void waitForElement(WebElement wb)
+{
+	   WebDriverWait wait=new WebDriverWait(driver,30);
+	   wait.until(ExpectedConditions.elementToBeClickable(wb));
+	   
+}
+public static void fileupload(String fpath) throws AWTException
+{
+	   
+	   StringSelection path=new StringSelection(fpath);//select
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(path,null);
+		Robot r=new Robot();
+		r.delay(5000);
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V); 
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		r.keyRelease(KeyEvent.VK_V);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+}
+}
 
